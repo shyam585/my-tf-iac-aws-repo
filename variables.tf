7d0eab8e494476466633c405813a19fe3c928580
shyam585 <67233183+shyam585@users.noreply.github.com>
2022-05-05 21:53:01 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "MY-DEMO"
+  default     =  "projectterraform"
 }
 
 variable "instance_type" {
